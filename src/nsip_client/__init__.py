783f7b014f1cb7fc8d98239db75be4bb2ab4accc
Allen R <bob@epicpastures.com>
2025-12-21 00:36:32 -0500

    chore: bump version (patch release)

--- src/nsip_client/__init__.py
+++ src/nsip_client/__init__.py
@@ -6,7 +6,7 @@ from .client import NSIPClient
 from .exceptions import NSIPAPIError, NSIPError, NSIPNotFoundError
 from .models import AnimalDetails, Lineage, Progeny, SearchCriteria
 
-__version__ = "1.3.20"
+__version__ = "1.3.21"
 __all__ = [
     "NSIPClient",
     "SearchCriteria",
